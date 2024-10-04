f722388236f635632cf83f8c4eefeea3659de9b5
Sayan Chakraborty <sayanchakraborty885@gmail.com>
2024-10-05 01:55:41 +0530

    First Commit

---
+++ variable.js
@@ -0,0 +1,23 @@
+// var name = "Keanu Reeves";
+// var name = "John cena";
+// console.log(name) // John Cena
+
+
+
+// let name = "Keanu Reeves";
+// let name = "John cena";
+// console.log(name);
+// output :- Identifier 'name' has already been declared
+
+
+
+// let name = "Keanu Reeves";
+// name = "John cena"
+// console.log(name); // John cena
+
+
+
+
+const name = "Keanu Reeves";
+name = "John cena"
+console.log(name); //TypeError: Assignment to constant variable.
