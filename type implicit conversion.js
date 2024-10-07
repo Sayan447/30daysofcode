afb7882b62e0968c562a407ac5e3e814e8f6836b
Sayan Chakraborty <sayanchakraborty885@gmail.com>
2024-10-08 00:20:27 +0530

    impicit

---
+++ type implicit conversion.js
@@ -0,0 +1,32 @@
+// result =  '3' + 2
+// result =  '3' + true
+// console.log(result); //32 , 3true
+// console.log(typeof result ); //string
+
+// result1 =  '3' + null
+// console.log(result1); //3null
+// console.log(typeof result1 ); //string
+
+// result2 =  '3' + undefined
+// console.log(result2); //3undefined
+// console.log(typeof result2 ); //string
+
+
+
+
+// result1 =  '3' - 2 
+// console.log(result1); // 1
+// console.log(typeof result1 );  //number
+
+// result =  '3' - null
+// console.log(result); // 3
+// console.log(typeof result );  //number
+
+
+// result1 =  '3' - true
+// console.log(result1); // 2
+// console.log(typeof result1 ); //number
+
+result1 =  '3' - 'hello'
+console.log(result1); // NaN
+console.log(typeof result1 ); //number
