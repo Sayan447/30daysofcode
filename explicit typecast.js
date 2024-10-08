91bbfdfb0adb18a1defe96b74fb5d7e439b1c194
Sayan Chakraborty <sayanchakraborty885@gmail.com>
2024-10-08 10:29:00 +0530

    operator

---
+++ explicit typecast.js
@@ -0,0 +1,23 @@
+// result = '3';
+// result = Number(result);
+// console.log(result); // 3
+// console.log(typeof result); // number
+
+
+// result = null;
+// result = Number(result);
+// console.log(result); // 0
+// console.log(typeof result); // number
+
+
+// result = 20.020;
+// result = parseInt(result);
+// console.log(result); // 20
+// console.log(typeof result); //number
+
+
+
+result = -20.002;
+result = parseFloat(result);
+console.log(result); // -20.002
+console.log(typeof result); //number
