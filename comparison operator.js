9965f872f0413f7780704228abf8ff5e26dc4840
Sayan Chakraborty <sayanchakraborty885@gmail.com>
2024-10-08 11:05:08 +0530

    comparison operator

---
+++ comparison operator.js
@@ -0,0 +1,8 @@
+a = 5;
+b = 6;
+console.log(a==b); // false
+console.log(a!=b); // true
+console.log(a>b); // false
+console.log(a<b); // true
+console.log(a>=b); // false
+console.log(a<=b); // true
