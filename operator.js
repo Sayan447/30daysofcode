7f814323820be08c8d600172e7441b072f1312da
Sayan Chakraborty <sayanchakraborty885@gmail.com>
2024-10-08 10:56:54 +0530

    prefix and postfix

--- operator.js
+++ operator.js
@@ -1,6 +1,14 @@
-console.log(10 + 5);
-console.log(10 - 5);
-console.log(10 * 5);
-console.log(10 / 5);
-console.log(11 % 5);
-console.log(10 ** 5);
+// console.log(10 + 5);
+// console.log(10 - 5);
+// console.log(10 * 5);
+// console.log(10 / 5);
+// console.log(11 % 5);
+// console.log(10 ** 5);
+
+
+
+let a = 1
+// a++ postfix
+// console.log(a++); // 1
+console.log(++a); // prefix - 2
+
