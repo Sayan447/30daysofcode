9467b879670e41244b5b883240d4ed3c332f4027
Sayan Chakraborty <sayanchakraborty885@gmail.com>
2024-10-14 09:30:06 +0530

    completed

--- comparison operator.js
+++ comparison operator.js
@@ -1,8 +1,20 @@
-a = 5;
-b = 6;
-console.log(a==b); // false
-console.log(a!=b); // true
-console.log(a>b); // false
-console.log(a<b); // true
-console.log(a>=b); // false
-console.log(a<=b); // true
+// a = 5;
+// b = 6;
+// console.log(a==b); // false
+// console.log(a!=b); // true
+// console.log(a>b); // false
+// console.log(a<b); // true
+// console.log(a>=b); // false
+// console.log(a<=b); // true
+
+ 
+
+
+a = '6'
+b = 6
+console.log(a == b); // true
+
+console.log(a!=b); // false
+
+console.log(a === b); // false
+
