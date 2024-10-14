b627dad2eab4dbaf468beda8f2b22f2f7a5cfc7b
Sayan Chakraborty <sayanchakraborty885@gmail.com>
2024-10-14 10:11:25 +0530

    conditional

---
+++ conditional statement .js
@@ -0,0 +1,21 @@
+// conditional 
+
+let age = 20;
+let name = 'jooe'
+
+if(age >= 18){
+    if(name == "john"){
+        console.log("You are eligible");
+    }
+    else{
+        console.log("Name is not found");
+        
+    }
+    
+}
+
+else{
+    console.log("You are not eligible");
+}
+
+// output : - You are not eligible
