8bd6539f91c553a0dcd9994c0fca68f9f62434a6
Sayan Chakraborty <sayanchakraborty885@gmail.com>
2024-10-15 15:15:02 +0530

    js loop

---
+++ loop.js
@@ -0,0 +1,21 @@
+// for loop
+
+// for(let i = 0; i<=10 ; i++){
+//     console.log(`2 x ${i} = ${2*i}`);
+// }
+
+
+// while loop
+// let i = 1;
+// while(i<=10){
+//     console.log(`2 x ${i} = ${2*i}`);
+//     i++;
+// }
+
+
+// do-while
+let i = 1;
+do{
+    console.log(`2 x ${i} = ${2*i}`);
+    i++;
+}while(i<=10)
