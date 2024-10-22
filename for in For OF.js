a4b994d097e7198a44f368a27ee078d698b41921
Sayan Chakraborty <sayanchakraborty885@gmail.com>
2024-10-23 00:34:12 +0530

    loop related

---
+++ for in For OF.js
@@ -0,0 +1,24 @@
+let arr = [10,20,30,40]
+
+for(let ar in arr){
+    // console.log(ar);
+    //0
+    // 1
+    // 2
+    // 3
+    // console.log(arr[ar]);
+    // 10
+    // 20
+    // 30
+    // 40
+}
+
+
+for( let ar of arr){
+    console.log(ar);
+    // 10
+    // 20
+    // 30
+    // 40
+    
+}
